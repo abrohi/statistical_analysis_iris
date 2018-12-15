021ba4c4ad886a3ad8f83980587ede9e76e53a10
abrohi <brohiamir@gmail.com>
2018-12-15 16:18:35 +0000

    include summary statistics and plots

--- code/etl.py
+++ code/etl.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
 
 class data_analysis():
     """
@@ -19,3 +21,43 @@ class data_analysis():
         dataset = pd.read_csv(url, names= names, index_col=None)
 
         return dataset
+    
+    
+    def stats_summary(data):
+        """
+        Applying summary statistics on data 
+        """
+        stats = data.describe()
+        
+        return stats
+    
+    def class_dist(data):
+        """
+        This function shows how the classes are distributed across the data-set
+        """
+        group_data = data.groupby('class').size()
+        return group_data
+    
+    def univariate_plt(data, plot_type):
+        """
+        Function will plot a chart for individual variable.
+        plot_type variable will contain either 'hist' or 'box',
+        for example etl.data_analysis.univariate(data, 'hist') for
+        histogram.
+        """
+        
+        data.plot(kind = plot_type, subplots=True, layout=(2,2),
+                     sharex=False, sharey= False)
+        plt.show()
+     
+    def multivariate_plt(data):
+        """
+        Plots the interaction between the different variables using
+        scatter matrix from pandas plotting module.
+        """
+        scatter_matrix(data, alpha=0.7)
+        plt.show()
+    
+    
+   
+    
